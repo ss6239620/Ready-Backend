5b632fd01b74d34e8488f372c5900ea2ee65a302
ss6239620 <ss6239620@gmail.com>
2024-12-10 15:56:21 +0530

    added production db

--- db/index.js
+++ db/index.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+let mongo_uri
+
+if (process.env.NODE_ENV === 'development') {
+    mongo_uri = process.env.MONGO_DB_DEVLOPMENT_DATABASE
+} else {
+    mongo_uri = process.env.MONGO_DB_PRODUCTION_DATABASE
+}
 
 const connectToMongo = () => {
     mongoose.connect("mongodb://0.0.0.0:27017/Ready").then(() => {
