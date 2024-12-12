698528a3a86dab5e718c4128aba71aada02abcdf
ss6239620 <ss6239620@gmail.com>
2024-12-12 21:27:00 +0530

    trying to sync glitch with github

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 // Configure CORS options
 const corsOptions = {
-    origin: ['http://localhost:3000','https://ready-ruddy.vercel.app'], // Replace with your frontend URL
+    origin: ['http://localhost:3000', 'https://ready-ruddy.vercel.app'], // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Enable cookies and authentication
 };
@@ -24,12 +24,46 @@ app.use(cors(corsOptions));
 
 const dir = './uploads';
 
-if (!fs.existsSync(dir)){
+if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
 }
 
 
 connectToMongo()
+
+//server github-to-glitch syncing
+const cmd = require('node-cmd')
+const crypto = require('crypto')
+
+const verifySignature = (req, res, next) => {
+    const payload = JSON.stringify(req.body)
+    const hmac = crypto.createHmac('sha1', process.env.GITHUB_SECRET)
+    const digest = 'sha1=' + hmac.update(payload).digest('hex')
+    const checksum = req.headers['x-hub-signature']
+
+    if (!checksum || !digest || checksum !== digest) {
+        return res.status(403).send('auth failed')
+    }
+
+    return next()
+}
+
+// Github webhook listener
+app.post('/git', verifySignature, (req, res) => {
+    if (req.headers['x-github-event'] == 'push') {
+        cmd.get('bash git.sh', (err, data) => {
+            if (err) return console.log(err)
+            console.log(data)
+            return res.status(200).send(data)
+        })
+    } else if (req.headers['x-github-event'] == 'ping') {
+        return res.status(200).send('PONG')
+    } else {
+        return res.status(200).send('Unsuported Github event. Nothing done.')
+    }
+})
+
+//server github-to-glitch syncing End Here
 
 app.get('/', (req, res) => {
     res.status(200).send("Server is active")
